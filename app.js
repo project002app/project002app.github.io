f42268ced4b3bcb1587e1292db6aad7ce21e5006
Pavan-da-Coder <61098154+Pavan-da-Coder@users.noreply.github.com>
2021-11-27 10:41:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -50,13 +50,13 @@ button.addEventListener('click', () => {
 });
 //This thingy thingy thingifieriyer makes notification
 function randomNotification() {
-    const randomItem = Math.floor(Math.random() * games.length);
-    const notifTitle = games[randomItem].name;
-    const notifBody = `Created by ${games[randomItem].author}.`;
-    const notifImg = `data/img/${games[randomItem].slug}.jpg`;
+    const randomItem = Math.floor(Math.random() * content.length);
+    const notifTitle = content[randomItem].scheduletype;
+    const notifBody = `Created by ${content[randomItem].period1}.`;
+    //const notifImg = `data/img/${games[randomItem].slug}.jpg`;
     const options = {
       body: notifBody,
-      icon: notifImg,
+      //icon: notifImg,
     };
     new Notification(notifTitle, options);
     setTimeout(randomNotification, 30000);
