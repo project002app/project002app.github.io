8e3bcb665d692a9b24f7e7771cc228217b6bcdb3
Pavan-da-Coder <61098154+Pavan-da-Coder@users.noreply.github.com>
2021-11-27 10:32:33 -0500

    Add everything again

---
+++ app.js
@@ -0,0 +1,64 @@
+//Note: In line 3 with the image, you need placeholder image inside data => contentimages folder
+//Also I removed this thingy "data-src='data/img/SLUG.jpg'" from the image tag, its just data-* whatever stuff, it weird but VERY cool
+//https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset
+//https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/data-*
+const template = `<article>
+  <img src='data/contentimages/placeholder.png' alt='NAME'>
+  <h3>#POS. NAME</h3>
+  <ul>
+  <li><span>Schedule:</span> <strong>SCHEDULETYPE</strong></li>
+  <li><span>Period 1:</span> <strong>PERIOD1</strong></li>
+  <li><span>Period 1:</span> <strong>PERIOD2</strong></li>
+  <li><span>Period 1:</span> <strong>PERIOD3</strong></li>
+  <li><span>Period 1:</span> <strong>PERIOD4</strong></li>
+  <li><span>Period 1:</span> <strong>PERIOD5</strong></li>
+  <li><span>Period 1:</span> <strong>PERIOD6</strong></li>
+  <li><span>Period 1:</span> <strong>PERIOD7</strong></li>
+  <li><span>Period 1:</span> <strong>PERIOD8</strong></li>
+  </ul>
+</article>`;
+let content = '';
+for (let i = 0; i < games.length; i++) {
+  let entry = template.replace(/POS/g, (i + 1))
+    .replace(/SCHEDULETYPE/g, content[i].scheduletype)
+    .replace(/PERIOD1/g, content[i].period1)
+    .replace(/PERIOD2/g, content[i].period2)
+    .replace(/PERIOD3/g, content[i].period3)
+    .replace(/PERIOD4/g, content[i].period4)
+    .replace(/PERIOD5/g, content[i].period5)
+    .replace(/PERIOD6/g, content[i].period6)
+    .replace(/PERIOD7/g, content[i].period7)
+    .replace(/PERIOD8/g, content[i].period8)
+  entry = entry.replace('<a href=\'http:///\'></a>', '-');
+  content += entry;
+}
+document.getElementById('content').innerHTML = content;
+
+
+//NOW, for THE SERVICE WORKER!!!!!!!!
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/Project 002 Alpha/sw.js");
+  }
+//This asks for permission to send notification and...sends a notification function in the end
+const button = document.getElementById('notifications');
+button.addEventListener('click', () => {
+    Notification.requestPermission().then((result) => {
+        if (result === 'granted') {
+            randomNotification();
+        }
+    });
+});
+//This thingy thingy thingifieriyer makes notification
+function randomNotification() {
+    const randomItem = Math.floor(Math.random() * games.length);
+    const notifTitle = games[randomItem].name;
+    const notifBody = `Created by ${games[randomItem].author}.`;
+    const notifImg = `data/img/${games[randomItem].slug}.jpg`;
+    const options = {
+      body: notifBody,
+      icon: notifImg,
+    };
+    new Notification(notifTitle, options);
+    setTimeout(randomNotification, 30000);
+  }
+  
---
+++ data/content.js
@@ -0,0 +1,61 @@
+  var content = [
+      {
+        scheduletype: 'Regular',
+        period1: 'aregular',
+        period2: 'b',
+        period3: 'c',
+        period4: 'd',
+        period5: 'e',
+        period6: 'f',
+        period7: 'g',
+        period8: 'h',
+      },
+      {
+        scheduletype: 'Monday',
+        period1: 'a2monday',
+        period2: 'b2',
+        period3: 'c2',
+        period4: 'd2',
+        period5: 'e2',
+        period6: 'f2',
+        period7: 'g2',
+        period8: 'h2',
+      },
+      {
+        scheduletype: 'Pep Rally',
+        period1: 'a3peprally',
+        period2: 'b3',
+        period3: 'c3',
+        period4: 'd3',
+        period5: 'e3',
+        period6: 'f3',
+        period7: 'g3',
+        period8: 'h3',
+      }
+  ];
+
+  //This is the code from the MDN tutorial/doc
+  /*var games = [
+    {
+      slug: 'lost-in-cyberspace',
+      name: 'Lost in Cyberspace',
+      author: 'Zosia and Bartek',
+      twitter: 'bartaz',
+      website: '',
+    },
+    {
+      slug: 'vernissage',
+      name: 'Vernissage',
+      author: 'Platane',
+      twitter: 'platane_',
+      website: 'github.com/Platane',
+    },
+    // ...
+    {
+      slug: 'emma-3d',
+      name: 'Emma-3D',
+      author: 'Prateek Roushan',
+      twitter: '',
+      website: '',
+    }
+  ];*/
