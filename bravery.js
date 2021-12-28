85cf7646558545ef6031d9a1de45e2dccac04772
projectpavanapp <pavan936@yahoo.com>
2021-12-28 15:34:51 -0500

    Working on the tables for the schedules

--- bravery.js
+++ bravery.js
@@ -3,17 +3,27 @@ function supremefunctionofhope() {
     console.log("I am Dr. Frankenstein");
 }
 //Start of the right side info holders
-const regular = document.querySelector('.regularinfoholder');
+//const regular = document.querySelector('.regularinfoholder');
 function regularfunction() {
-    //const regular = document.querySelector('.regularinfoholder');
+    const regular = document.querySelector('.regularinfoholder');
     /*if(document.querySelector('.mondayinfoholder').style.display === "block"){
         document.querySelector('.mondayinfoholder').style.display = "none";
     }*/
-
-
     if(regular.style.display === "block"){
         regular.style.display = "none";
+        //make everything else none
+        document.querySelector('.mondayinfoholder').style.display = "none";
+        document.querySelector('.peprallyinfoholder').style.display = "none";
+        document.querySelector('.specialinfoholder').style.display = "none";
+        document.querySelector('.homeroominfoholder').style.display = "none";
+        document.querySelector('.clubinfoholder').style.display = "none";
     }else{
+        //make everything else none
+        document.querySelector('.mondayinfoholder').style.display = "none";
+        document.querySelector('.peprallyinfoholder').style.display = "none";
+        document.querySelector('.specialinfoholder').style.display = "none";
+        document.querySelector('.homeroominfoholder').style.display = "none";
+        document.querySelector('.clubinfoholder').style.display = "none";
         regular.style.display = "block";
         
     }
@@ -22,7 +32,17 @@ function mondayfunction() {
     const monday = document.querySelector('.mondayinfoholder');
     if(monday.style.display === "block"){
         monday.style.display = "none";
+        document.querySelector('.regularinfoholder').style.display = "none";
+        document.querySelector('.peprallyinfoholder').style.display = "none";
+        document.querySelector('.specialinfoholder').style.display = "none";
+        document.querySelector('.homeroominfoholder').style.display = "none";
+        document.querySelector('.clubinfoholder').style.display = "none";
     }else{
+        document.querySelector('.regularinfoholder').style.display = "none";
+        document.querySelector('.peprallyinfoholder').style.display = "none";
+        document.querySelector('.specialinfoholder').style.display = "none";
+        document.querySelector('.homeroominfoholder').style.display = "none";
+        document.querySelector('.clubinfoholder').style.display = "none";
         monday.style.display = "block";
         
     }
@@ -31,7 +51,17 @@ function peprallyfunction() {
     const peprally = document.querySelector('.peprallyinfoholder');
     if(peprally.style.display === "block"){
         peprally.style.display = "none";
+        document.querySelector('.mondayinfoholder').style.display = "none";
+        document.querySelector('.regularinfoholder').style.display = "none";
+        document.querySelector('.specialinfoholder').style.display = "none";
+        document.querySelector('.homeroominfoholder').style.display = "none";
+        document.querySelector('.clubinfoholder').style.display = "none";
     }else{
+        document.querySelector('.mondayinfoholder').style.display = "none";
+        document.querySelector('.regularinfoholder').style.display = "none";
+        document.querySelector('.specialinfoholder').style.display = "none";
+        document.querySelector('.homeroominfoholder').style.display = "none";
+        document.querySelector('.clubinfoholder').style.display = "none";
         peprally.style.display = "block";
         
     }
@@ -40,7 +70,17 @@ function specialfunction() {
     const special = document.querySelector('.specialinfoholder');
     if(special.style.display === "block"){
         special.style.display = "none";
+        document.querySelector('.mondayinfoholder').style.display = "none";
+        document.querySelector('.peprallyinfoholder').style.display = "none";
+        document.querySelector('.regularinfoholder').style.display = "none";
+        document.querySelector('.homeroominfoholder').style.display = "none";
+        document.querySelector('.clubinfoholder').style.display = "none";
     }else{
+        document.querySelector('.mondayinfoholder').style.display = "none";
+        document.querySelector('.peprallyinfoholder').style.display = "none";
+        document.querySelector('.regularinfoholder').style.display = "none";
+        document.querySelector('.homeroominfoholder').style.display = "none";
+        document.querySelector('.clubinfoholder').style.display = "none";
         special.style.display = "block";
         
     }
@@ -49,7 +89,17 @@ function homeroomfunction() {
     const homeroom = document.querySelector('.homeroominfoholder');
     if(homeroom.style.display === "block"){
         homeroom.style.display = "none";
+        document.querySelector('.mondayinfoholder').style.display = "none";
+        document.querySelector('.peprallyinfoholder').style.display = "none";
+        document.querySelector('.specialinfoholder').style.display = "none";
+        document.querySelector('.regularinfoholder').style.display = "none";
+        document.querySelector('.clubinfoholder').style.display = "none";
     }else{
+        document.querySelector('.mondayinfoholder').style.display = "none";
+        document.querySelector('.peprallyinfoholder').style.display = "none";
+        document.querySelector('.specialinfoholder').style.display = "none";
+        document.querySelector('.regularinfoholder').style.display = "none";
+        document.querySelector('.clubinfoholder').style.display = "none";
         homeroom.style.display = "block";
         
     }
@@ -58,7 +108,17 @@ function clubfunction() {
     const club = document.querySelector('.clubinfoholder');
     if(club.style.display === "block"){
         club.style.display = "none";
+        document.querySelector('.mondayinfoholder').style.display = "none";
+        document.querySelector('.peprallyinfoholder').style.display = "none";
+        document.querySelector('.specialinfoholder').style.display = "none";
+        document.querySelector('.homeroominfoholder').style.display = "none";
+        document.querySelector('.regularinfoholder').style.display = "none";
     }else{
+        document.querySelector('.mondayinfoholder').style.display = "none";
+        document.querySelector('.peprallyinfoholder').style.display = "none";
+        document.querySelector('.specialinfoholder').style.display = "none";
+        document.querySelector('.homeroominfoholder').style.display = "none";
+        document.querySelector('.regularinfoholder').style.display = "none";
         club.style.display = "block";
         
     }
