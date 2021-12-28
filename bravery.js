9715b9f5be2776fcc95852ca027e72b9568d9cd5
projectpavanapp <pavan936@yahoo.com>
2021-12-28 11:38:08 -0500

    Working on the JavaScript

--- bravery.js
+++ bravery.js
@@ -2,15 +2,66 @@ console.log("hello from the land of the code");
 function supremefunctionofhope() {
     console.log("I am Dr. Frankenstein");
 }
-function testfunction() {
-    const racecar = document.querySelector('.scheduleinfoholder')
-    
-    if(racecar.style.display === "block"){
-        racecar.style.display = "none";
+//Start of the right side info holders
+function regularfunction() {
+    const regular = document.querySelector('.regularinfoholder');
+    if(document.querySelector('.mondayinfoholder').style.display === "block"){
+        document.querySelector('.mondayinfoholder').style.display = "none";
+    }
+    if(regular.style.display === "block"){
+        regular.style.display = "none";
     }else{
-        racecar.style.display = "block";
+        regular.style.display = "block";
         
     }
-    //This is the old way, with ID, and that is torture
-    //document.getElementById("hello").style.display = "none";
 }
+function mondayfunction() {
+    const monday = document.querySelector('.mondayinfoholder');
+    if(monday.style.display === "block"){
+        monday.style.display = "none";
+    }else{
+        monday.style.display = "block";
+        
+    }
+}
+function peprallyfunction() {
+    const peprally = document.querySelector('.peprallyinfoholder');
+    if(peprally.style.display === "block"){
+        peprally.style.display = "none";
+    }else{
+        peprally.style.display = "block";
+        
+    }
+}
+function specialfunction() {
+    const special = document.querySelector('.specialinfoholder');
+    if(special.style.display === "block"){
+        special.style.display = "none";
+    }else{
+        special.style.display = "block";
+        
+    }
+}
+function homeroomfunction() {
+    const homeroom = document.querySelector('.homeroominfoholder');
+    if(homeroom.style.display === "block"){
+        homeroom.style.display = "none";
+    }else{
+        homeroom.style.display = "block";
+        
+    }
+}
+function clubfunction() {
+    const club = document.querySelector('.clubinfoholder');
+    if(club.style.display === "block"){
+        club.style.display = "none";
+    }else{
+        club.style.display = "block";
+        
+    }
+}
+
+
+
+
+//End of the right side info holders
