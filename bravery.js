599731de8a8d4ed650d2f9cf3d1a36790d0b7990
projectpavanapp <pavan936@yahoo.com>
2021-12-28 10:26:36 -0500

    Superpublish

--- bravery.js
+++ bravery.js
@@ -2,3 +2,15 @@ console.log("hello from the land of the code");
 function supremefunctionofhope() {
     console.log("I am Dr. Frankenstein");
 }
+function testfunction() {
+    const racecar = document.querySelector('.scheduleinfoholder')
+    
+    if(racecar.style.display === "none"){
+        racecar.style.display = "block";
+    }else{
+        racecar.style.display = "none";
+        
+    }
+    //This is the old way, with ID, and that is torture
+    //document.getElementById("hello").style.display = "none";
+}
