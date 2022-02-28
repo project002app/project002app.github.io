6a4663a629459fc614741adbbedd2eb1a060af7b
project002app <95101347+project002app@users.noreply.github.com>
2022-02-28 09:01:57 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,16 +4,16 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('supremecacher').then(function(something) {
       return something.addAll([
-        'https://projectpavanapp.github.io/oldindex.html',
-        'https://projectpavanapp.github.io/index.html',
-        'https://projectpavanapp.github.io/schedules.html',
-        'https://projectpavanapp.github.io/calendars.html',
-        'https://projectpavanapp.github.io/homestyle.css',
-        'https://projectpavanapp.github.io/schedulestyle.css',
-        'https://projectpavanapp.github.io/calendarstyle.css',
-        'https://projectpavanapp.github.io/app.js',
-        'https://projectpavanapp.github.io/favicon.ico',
-        'https://projectpavanapp.github.io/project002.webmanifest'
+        'https://project002app.github.io/oldindex.html',
+        'https://project002app.github.io/index.html',
+        'https://project002app.github.io/schedules.html',
+        'https://project002app.github.io/calendars.html',
+        'https://project002app.github.io/homestyle.css',
+        'https://project002app.github.io/schedulestyle.css',
+        'https://project002app.github.io/calendarstyle.css',
+        'https://project002app.github.io/app.js',
+        'https://project002app.github.io/favicon.ico',
+        'https://project002app.github.io/project002.webmanifest'
       ]);
     })
   );
@@ -39,15 +39,15 @@ self.addEventListener('install', function(e) {
   //Lists the files except the main mass image files to be cached
   /*const cacheName = 'Project 002 Cache';
   const appShellFiles = [
-        'https://projectpavanapp.github.io',
-        'https://projectpavanapp.github.io/index.html',
-        'https://projectpavanapp.github.io/app.js',
-        'https://projectpavanapp.github.io/favicon.ico',
-        'https://projectpavanapp.github.io/project002.webmanifest',
-        'https://projectpavanapp.github.io/style.css',
+        'https://project002app.github.io',
+        'https://project002app.github.io/index.html',
+        'https://project002app.github.io/app.js',
+        'https://project002app.github.io/favicon.ico',
+        'https://project002app.github.io/project002.webmanifest',
+        'https://project002app.github.io/style.css',
         //Now to cache the images
-        //'https://projectpavanapp.github.io/img for the main/formula e.jpg',
-        //'https://projectpavanapp.github.io/icons for web app/computer 16x16.png',
+        //'https://project002app.github.io/img for the main/formula e.jpg',
+        //'https://project002app.github.io/icons for web app/computer 16x16.png',
   ];*/
   /*Uh note: do not cache content.js file bc MDN said "You may notice we haven't cached game.js.
   This is the file that contains the data we use when displaying our games. In reality this 
