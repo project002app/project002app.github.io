e4c03effd36bfe4d5da39f6f653da9996790ce0f
projectpavanapp <pavan936@yahoo.com>
2022-03-06 12:28:59 -0500

    Not gonna work

--- bravery.js
+++ bravery.js
@@ -2,26 +2,98 @@ function supremefunctionofhope() {
     console.log("I am Dr. Frankenstein");
 }
 
-// Add your App ID
-const app = new Realm.App({ id: application-0-drrgt });
+//This is not going to work
 
-async function loginAnonymous() {
-    // Create an anonymous credential
-    const credentials = Realm.Credentials.anonymous();
-    try {
-      // Authenticate the user
-      const user = await app.logIn(credentials);
-      // `App.currentUser` updates to match the logged in user
-      console.assert(user.id === app.currentUser.id);
-      return user;
-    } catch (err) {
-      console.error("Failed to log in", err);
-    }
-  }
-  const user = await loginAnonymous();
-  console.log("Successfully logged in!", user.id);
+
+import React from "react";
+import "./styles.css";
+
+// Import the MongoDB Realm Web SDK
+import * as Realm from "realm-web";
+
+// Connect to your MongoDB Realm app
+const REALM_APP_ID = "application-0-drrgt"; // e.g. myapp-abcde
+const app = new Realm.App({ id: REALM_APP_ID });
+
+// Create a component that displays the given user's details
+function UserDetail({ user }) {
+  return (
+    <div>
+      <h1>Logged in with anonymous id: {user.id}</h1>
+    </div>
+  );
+}
+
+// Create a component that lets an anonymous user log in
+function Login({ setUser }) {
+  const loginAnonymous = async () => {
+    const user = await app.logIn(Realm.Credentials.anonymous());
+    setUser(user);
+  };
+  return <button onClick={loginAnonymous}>Log In</button>;
+}
+
+const App = () => {
+  // Keep the logged in Realm user in local state. This lets the app re-render
+  // whenever the current user changes (e.g. logs in or logs out).
+  const [user, setUser] = React.useState(app.currentUser);
+
+  // If a user is logged in, show their details.
+  // Otherwise, show the login screen.
+  return (
+    <div className="App">
+      <div className="App-header">
+        {user ? <UserDetail user={user} /> : <Login setUser={setUser} />}
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
   
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //Start of the right side info holders
