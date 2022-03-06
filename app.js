4f274b2fa527aa53a9e1a86fe3bc978dc07cfc4d
projectpavanapp <pavan936@yahoo.com>
2022-03-06 14:21:49 -0500

    Somehow not gonna work at all

--- app.js
+++ app.js
@@ -1,4 +1,68 @@
 
+//DEFINITELY NOT GONNA WORK
+
+// Connect to your MongoDB Realm app
+const REALM_APP_ID = "application-0-drrgt"; // e.g. myapp-abcde
+const app = new Realm.App({ id: REALM_APP_ID });
+
+// Create a component that displays the given user's details
+function UserDetail({ user }) {
+  return (
+    <div>
+      <h1>Logged in with anonymous id: {user.id}</h1>
+    </div>
+    //Note: fix this
+  );
+}
+
+// Create a component that lets an anonymous user log in
+function Login({ setUser }) {
+  const loginAnonymous = async () => {
+    const user = await app.logIn(Realm.Credentials.anonymous());
+    setUser(user);
+  };
+  return <button onClick={loginAnonymous}>Log In</button>;
+}
+
+const App = () => {
+  // Keep the logged in Realm user in local state. This lets the app re-render
+  // whenever the current user changes (e.g. logs in or logs out).
+  //const [user, setUser] = React.useState(app.currentUser);
+
+  // If a user is logged in, show their details.
+  // Otherwise, show the login screen.
+  return (
+    <div className="App">
+      <div className="App-header">
+        {user ? <UserDetail user={user} /> : <Login setUser={setUser} />}
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Note: In line 3 with the image, you need placeholder image inside data => contentimages folder
 //Also I removed this thingy "data-src='data/img/SLUG.jpg'" from the image tag, its just data-* whatever stuff, it weird but VERY cool
 //https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset
--- bravery.js
+++ bravery.js
@@ -11,47 +11,6 @@ function supremefunctionofhope() {
 // Import the MongoDB Realm Web SDK
 //import * as Realm from "realm-web";
 
-// Connect to your MongoDB Realm app
-const REALM_APP_ID = "application-0-drrgt"; // e.g. myapp-abcde
-const app = new Realm.App({ id: REALM_APP_ID });
-
-// Create a component that displays the given user's details
-function UserDetail({ user }) {
-  return (
-    <div>
-      <h1>Logged in with anonymous id: {user.id}</h1>
-    </div>
-    //Note: fix this
-  );
-}
-
-// Create a component that lets an anonymous user log in
-function Login({ setUser }) {
-  const loginAnonymous = async () => {
-    const user = await app.logIn(Realm.Credentials.anonymous());
-    setUser(user);
-  };
-  return <button onClick={loginAnonymous}>Log In</button>;
-}
-
-const App = () => {
-  // Keep the logged in Realm user in local state. This lets the app re-render
-  // whenever the current user changes (e.g. logs in or logs out).
-  //const [user, setUser] = React.useState(app.currentUser);
-
-  // If a user is logged in, show their details.
-  // Otherwise, show the login screen.
-  return (
-    /*<div className="App">
-      <div className="App-header">
-        {user ? <UserDetail user={user} /> : <Login setUser={setUser} />}
-      </div>
-    </div>*/
-    console.log("Uhh finished???")
-  );
-};
-
-export default App;
 
   
 
